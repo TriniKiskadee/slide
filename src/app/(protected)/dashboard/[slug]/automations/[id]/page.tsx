65a9436d14166b51771cacc09d642abc637a82db
TriniKiskadee <renaudsennon@gmail.com>
2024-12-10 16:42:35 -0400

    updated gitignore

--- src/app/(protected)/dashboard/[slug]/automations/[id]/page.tsx
+++ src/app/(protected)/dashboard/[slug]/automations/[id]/page.tsx
@@ -5,6 +5,7 @@ import Trigger from "@/components/global/automations/trigger";
 import {getAutomationInfo} from "@/actions/automations";
 import {dehydrate, HydrationBoundary, QueryClient} from "@tanstack/react-query";
 import {PrefetchUserAutomation} from "@/react-query/prefetch";
+import { onCurrentUser } from '@/actions/user';
 
 
 type AutomationIdPageProps = {
@@ -26,10 +27,11 @@ const AutomationIdPage = async ({params}: AutomationIdPageProps) => {
     const query = new QueryClient()
     await PrefetchUserAutomation(query, params.id)
 
+    console.log('Banana')
 
     return (
         <HydrationBoundary state={dehydrate(query)}>
-            <div className={'flex flex-col items-center gap-y-20'}>
+            <div className={'flex flex-col items-center gap-y-20 '}>
                 <AutomationBreadCrumb id={params.id}/>
                 <div className={'w-full lg:w-10/12 xl:w-6/12 p-5 rounded-xl flex flex-col bg-[#1D1D1D] gap-y-3'}>
                     <div className={'flex gap-x-2'}>
